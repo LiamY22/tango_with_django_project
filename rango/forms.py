d87c8824642ce3116ef113a6686cd6e126677000
Liam Yeates <3166020Y@student.gla.ac.uk>
2026-03-02 17:04:17 +0000

    Completed chapter 7 and passed all the tests. Made the new add_page.html and updated the category page to provide a link from the category page to the add page. Also, change the max length values so that they are not hard coded.

--- rango/forms.py
+++ rango/forms.py
@@ -1,8 +1,8 @@
 from django import forms
-from rango.models import Page, Category
+from rango.models import Page, Category, NAME_MAX_LENGTH, TITLE_MAX_LENGTH
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128,
+    name = forms.CharField(max_length=NAME_MAX_LENGTH,
             help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
@@ -15,7 +15,7 @@ class CategoryForm(forms.ModelForm):
         fields = ('name',)
 
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=128,
+    title = forms.CharField(max_length=TITLE_MAX_LENGTH,
             help_text="Please enter the title of the page.")
     url = forms.URLField(max_length=200,
             help_text="Please enter the URL of the page.")
@@ -33,3 +33,13 @@ class PageForm(forms.ModelForm):
         exclude = ('category',)
         # or specify the fields to include (don't include the category field).
         #fields = ('title', 'url', 'views')
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        if url and not url.startswith('http://') and not url.startswith('https://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
+
+        return cleaned_data
